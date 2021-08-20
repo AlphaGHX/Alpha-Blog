b92cd17ce1c9864be29944bf7416f3d277537582
AlphaGHX <ChenYuanHangOL@outlook.com>
2021-08-20 17:47:46 +0800

    fix: 删除了不必要的store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,22 +1,8 @@
 import { createStore } from 'vuex'
 
 export default createStore({
-  state: {
-    tocData: {},
-    markdownData: {},
-    nowPage: 0
-  },
-  mutations: {
-    setTocData(state, data) {
-      state.tocData = data
-    },
-    setMarkdownData(state, data) {
-      state.markdownData = data
-    },
-    setNowPage(state, data) {
-      state.nowPage = data
-    }
-  },
+  state: {},
+  mutations: {},
   actions: {},
   modules: {}
 })
